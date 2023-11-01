e20d947a2d666080dba5f01717a8f5ccfba30440
xg298 <xg298@cornell.edu>
2023-11-01 11:34:27 -0400

    Update script.js

--- week11/script.js
+++ week11/script.js
@@ -4,5 +4,23 @@ var map = new mapboxgl.Map({
 	style:'mapbox://styles/xg298/clofvsffe004g01pdgf5ogutn',
 	center:[-122.43796, 37.60837],
 	zoom:10
-}
-)
+})；
+
+map.on('load',function(){
+    map.addLayer({
+    	id:' inspection-layer',
+    	source:{
+    		type:'vector',
+    		url:'//studio.mapbox.com/tilesets/xg298.7povdqba'
+     	}
+     	'source-layer':'Historical_Restaurant_Scores_-6lme2b',
+        paint:{
+        	'fill-color':'#008F8C',
+        	'fill-opacity':0.5
+
+        }
+    	
+    	})
+    
+})
+
