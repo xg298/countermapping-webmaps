d693a76dbdb10965a7d0311db0ec826d4163accd
xg298 <xg298@cornell.edu>
2023-11-01 10:57:30 -0400

    week11

---
+++ week11/script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoieGcyOTgiLCJhIjoiY2xvZnZybDdiMGRkMTJrcmt0NWtxOGh3bCJ9.iFCcl0YymjvPMJDElFxsTw';
+var map = new mapboxgl.Map({
+	container:'xg298',
+	style:'mapbox://styles/xg298/clofvsffe004g01pdgf5ogutn',
+	center:[-122.43796, 37.60837],
+	zoom:10
+}
+)
